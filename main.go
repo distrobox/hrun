8734d9ce2f8cb1f78c02c225bfb1f237df574fb2
mirkobrombin <send@mirko.pm>
2024-02-28 10:45:07 +0100

    fix: server not closing

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"log"
 	"net"
 	"os"
 	"os/exec"
@@ -24,7 +24,7 @@ func main() {
 
 	// Print help message if the first argument is "-h" or "--help"
 	if len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help") {
-		fmt.Println(`Usage: hrun [options] command [args...]
+		log.Println(`Usage: hrun [options] command [args...]
 
 If command is "start", it starts the server. Otherwise, it starts the client
 and sends the command to the server. If no command is provided, it starts a
@@ -42,143 +42,175 @@ shell on the host.`)
 }
 
 func startServer() {
-	// create a listener for the server
+	// Create a listener for the server
 	listener, err := net.Listen("tcp", "127.0.0.1:8080")
 	if err != nil {
 		panic(err)
 	}
 	defer listener.Close()
-	fmt.Println("Server is running on 127.0.0.1:8080")
+	log.Printf("Server is running on 127.0.0.1:8080\n\n")
 
-	// accept connections from the client
-	for {
-		var conn net.Conn
-		conn, err = listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
-			continue
-		}
-
-		defer conn.Close()
-
-		// read the command from the client and split it into parts
-		reader := bufio.NewReader(conn)
-		command, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Failed to read command: ", err)
-			return
-		}
-		fmt.Printf("Received command: %s\n", command)
-		command = strings.TrimSpace(command)
-		cmdParts := strings.Fields(command)
-		if len(cmdParts) == 0 {
-			fmt.Fprintf(conn, "Invalid command\n")
-			return
-		}
-
-		// prepare a pty
-		var ptyMaster, ptySlave *os.File
-		ptyMaster, ptySlave, err = pty.Open()
-		if err != nil {
-			fmt.Println("Error creating PTY:", err)
-			conn.Close()
-			return
-		}
-		fmt.Println("PTY created")
-
-		// set up the channels to communicate with the host
-		go func() {
-			io.Copy(conn, ptyMaster)
-			ptyMaster.Close()
-			conn.Close()
-		}()
-		go func() {
-			io.Copy(ptyMaster, conn)
-			ptyMaster.Close()
-			conn.Close()
-		}()
-		fmt.Println("Channels set up")
-
-		// execute the command
-		cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
-		cmd.Stdin = ptySlave
-		cmd.Stdout = ptySlave
-		cmd.Stderr = ptySlave
-
-		// set the process group so that the termination signal is
-		// forwarded to the shell process
-		cmd.SysProcAttr = &syscall.SysProcAttr{}
-		cmd.SysProcAttr.Setctty = true
-		cmd.SysProcAttr.Setsid = true
-		cmd.SysProcAttr.Pdeathsig = syscall.SIGTERM
-
-		// start the shell process
-		if err = cmd.Start(); err != nil {
-			fmt.Println("Error starting shell:", err)
-			conn.Close()
-			return
-		}
-		fmt.Println("Shell started")
-
-		// we need to create a channel to handle termination signals
+	// Set up a signal handler to shut down the server
+	doneCh := make(chan struct{})
+	go func() {
 		sigCh := make(chan os.Signal, 1)
 		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
 
-		// setting up a goroutine to wait for the shell process to exit
-		go func() {
-			cmd.Wait()
-			ptySlave.Close()
-		}()
+		<-sigCh
+		log.Println("Shutdown signal received, closing server...")
+		close(doneCh)
+	}()
 
-		// a goroutine to handle termination signals
-		go func() {
-			<-sigCh
-			fmt.Println("Closing the connection and shell process...")
-			conn.Close()
-			syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
-		}()
-
-		// wait for the shell process to exit
-		cmd.Wait()
-		fmt.Println("Shell process exited")
+	// Accept connections and handle them
+	for {
+		select {
+		case <-doneCh:
+			log.Println("Shutting down server...")
+			return
+		case conn, ok := <-acceptConn(listener):
+			if !ok {
+				log.Println("Listener closed, shutting down server...")
+				return
+			}
+			go handleConnection(conn)
+		}
 	}
 }
 
+func acceptConn(listener net.Listener) <-chan net.Conn {
+	ch := make(chan net.Conn)
+	go func() {
+		defer close(ch)
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			return
+		}
+		ch <- conn
+	}()
+	return ch
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	// Read the command from the client and split it into parts
+	reader := bufio.NewReader(conn)
+	command, err := reader.ReadString('\n')
+	if err != nil {
+		log.Println("Failed to read command: ", err)
+		return
+	}
+	log.Printf("Received command: %s", command)
+	command = strings.TrimSpace(command)
+	cmdParts := strings.Fields(command)
+	if len(cmdParts) == 0 {
+		log.Println("No command provided")
+		return
+	}
+
+	// Prepare a pty
+	var ptyMaster, ptySlave *os.File
+	ptyMaster, ptySlave, err = pty.Open()
+	if err != nil {
+		log.Println("Error creating PTY:", err)
+		conn.Close()
+		return
+	}
+	log.Println("PTY created")
+
+	// Set up the channels to communicate with the host
+	go func() {
+		io.Copy(conn, ptyMaster)
+		ptyMaster.Close()
+		conn.Close()
+	}()
+	go func() {
+		io.Copy(ptyMaster, conn)
+		ptyMaster.Close()
+		conn.Close()
+	}()
+
+	// Execute the command
+	cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
+	cmd.Stdin = ptySlave
+	cmd.Stdout = ptySlave
+	cmd.Stderr = ptySlave
+
+	// Set the process group so that the termination signal is
+	// forwarded to the shell process
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	cmd.SysProcAttr.Setctty = true
+	cmd.SysProcAttr.Setsid = true
+	cmd.SysProcAttr.Pdeathsig = syscall.SIGTERM
+
+	// Start the shell process
+	if err = cmd.Start(); err != nil {
+		log.Println("Error starting shell:", err)
+		conn.Close()
+		return
+	}
+	log.Println("Shell started")
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// Setting up a goroutine to wait for the shell process to exit
+	go func() {
+		cmd.Wait()
+		ptySlave.Close()
+	}()
+
+	// Handle the termination signal
+	go func() {
+		<-sigCh
+		log.Println("Closing the connection and shell process...")
+		conn.Close()
+		syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+	}()
+
+	// Wait for the shell process to exit
+	cmd.Wait()
+	log.Println("Shell process exited")
+	ptySlave.Close()
+	log.Printf("Connection closed\n\n")
+}
+
 func startClient(command string) {
-	// connect to the server
+	// Connect to the server
 	conn, err := net.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 
-	// send the command to the server
+	// Send the command to the server
 	_, err = conn.Write([]byte(command + "\n"))
 	if err != nil {
-		fmt.Println("Error sending command to the server:", err)
+		log.Println("Error sending command to the server:", err)
 		return
 	}
 
-	// set the terminal to raw mode
+	// Set the terminal to raw mode
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
 		panic(err)
 	}
 	defer func() { _ = term.Restore(int(os.Stdin.Fd()), oldState) }()
 
-	// create a channel to communicate with the pty
+	// Create a channel to communicate with the pty
 	doneCh := make(chan struct{})
 	go func() {
 		_, err := io.Copy(conn, os.Stdin)
 		if err != nil {
-			fmt.Println("Error copying data to the server:", err)
+			log.Println("Error copying data to the server:", err)
 		}
 		close(doneCh)
 	}()
 	go func() {
 		_, err := io.Copy(os.Stdout, conn)
 		if err != nil {
-			fmt.Println("Error copying data from the server:", err)
+			log.Println("Error copying data from the server:", err)
 		}
 		close(doneCh)
 	}()
